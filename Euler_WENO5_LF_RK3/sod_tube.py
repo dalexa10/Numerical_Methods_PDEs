f5929897155ebbfa562f6b22e51b45f461f49d8b
Dario <dario.rodriguez9378@hotmail.com>
2023-04-21 18:22:32 -0500

    Code and figures updated

--- Euler_WENO5_LF_RK3/sod_tube.py
+++ Euler_WENO5_LF_RK3/sod_tube.py
@@ -16,6 +16,11 @@ def compute_pressure(rho, rho_u, E, gam=7/5):
     P = (gam - 1) * (E - 0.5 * (rho_u**2) / rho)
     return P
 
+
+# -----------------------------------------------------
+#               Initial condition function
+#                   Sod's Problem
+# -----------------------------------------------------
 def initial_fun(x, gam=7/5):
     """ Sets the initial value of multidimensional u vector as function of x
     as a Sod's problem instance """
@@ -36,6 +41,64 @@ def initial_fun(x, gam=7/5):
             P_vec[i] = compute_pressure(rho, rho_u, E)
     return u_vec, P_vec
 
+# -----------------------------------------------------
+#                 Boundary conditions
+#                   Sod's Problem
+# -----------------------------------------------------
+def left_BC():
+    rho = 1.
+    rho_u = 0.
+    E = 1./ (gam - 1)
+    u_l = np.array([rho, rho_u, E]).reshape([-1, 1])
+    return u_l
+
+def right_BC():
+    rho = 0.125
+    rho_u = 0.
+    E = 0.1 / (gam - 1)
+    u_r = np.array([rho, rho_u, E]).reshape([-1, 1])
+    return u_r
+
+
+# -----------------------------------------------------
+#     Coefficients for WENO squeme for p = 2 & p = 3
+# -----------------------------------------------------
+def cr_coefficients(p):
+    if p == 2:
+        c_rj = np.array([[3/2, -1/2],
+                         [1/2, 1/2],
+                         [-1/2, 3/2]])
+    else:
+        c_rj = np.array([[11/6, -7/6, 1/3],
+                         [1/3, 5/6, -1/6],
+                         [-1/6, 5/6, 1/3],
+                         [1/3, -7/6, 11/6]])
+    return c_rj
+
+
+def dr_coefficients(p):
+    if p == 2:
+        d_r = np.array([2/3, 1/3])
+    else:
+        d_r = np.array([3/10, 6/10, 1/10])
+    return d_r
+
+def add_BC_conditions_to_ghost_cells(u_vec, p):
+    """ Add boundary conditions to edge cells (ghost) depending on p degree of
+    interpolating polynomial """
+    if p == 2:
+        u_vec[:, :2] = left_BC() * np.ones([3, 2])
+        u_vec[:, -2:] = right_BC() * np.ones([3, 2])
+    else:
+        u_vec[:, :3] = left_BC() * np.ones([3, 3])
+        u_vec[:, -3:] = right_BC() * np.ones([3, 3])
+
+    return u_vec
+
+
+# -----------------------------------------------------
+#               Numerical functions
+# -----------------------------------------------------
 def flux_fun(u_vec):
     """ Compute the flux vector from given u vector (states) """
     rho, rho_u, E = u_vec[0, :], u_vec[1, :], u_vec[2, :]
@@ -45,10 +108,6 @@ def flux_fun(u_vec):
                     (E + P) * rho_u / rho))
     return f_u
 
-
-# -----------------------------------------------------
-#               Numerical functions
-# -----------------------------------------------------
 def compute_eigenvalues(u_vec, gam=7/5):
     """ Compute the eigenvalues of the mass matrix of the hyperbolic system
     of the 1D Euler equations """
@@ -65,33 +124,19 @@ def compute_lambda_max(lamb_v):
     return max_l
 
 def compute_alfa_LF(l_v_minus, l_v_plus):
-    """ Compute the factor alpha k + 1/2 """
+    """ Compute the factor alpha k + 1/2 for the Lax-Fridrich flux function"""
     max_l_minus = compute_lambda_max(l_v_minus)
     max_l_plus = compute_lambda_max(l_v_plus)
     alpha = np.maximum(max_l_minus, max_l_plus)
     return alpha
 
-def cr_coefficiens(p):
-    if p == 2:
-        c_rj = np.array([[3/2, -1/2],
-                         [1/2, 1/2],
-                         [-1/2, 3/2]])
-    else:
-        c_rj = np.array([[11/6, -7/6, 1/3],
-                         [1/3, 5/6, -1/6],
-                         [-1/6, 5/6, 1/3],
-                         [1/3, -7/6, 11/6]])
-    return c_rj
-
-def dr_coefficients(p):
-    if p == 2:
-        d_r = np.array([2/3, 1/3])
-    else:
-        d_r = np.array([3/10, 6/10, 1/10])
-    return d_r
-
 def compute_polynomials(u_vec, p, side):
-    c_rj = cr_coefficiens(p)
+    """
+    Compute the interpolating polynomials phi for the cell interfaces
+        right: k + 1/2
+        left: k - 1/2
+    """
+    c_rj = cr_coefficients(p)
     global K, Km2, Km1, Kp2, Kp1
     if side == 'right':
         p0 = c_rj[1, 0] * u_vec[:, K] + c_rj[1, 1] * u_vec[:, Kp1] + c_rj[1, 2] * u_vec[:, Kp2]
@@ -105,6 +150,8 @@ def compute_polynomials(u_vec, p, side):
     return p0, p1, p2
 
 def compute_beta(u_vec, p):
+    """ Calculate smoothness indicators beta """
+
     global K, Km2, Km1, Kp2, Kp1
     if p == 2:
         beta0 = (u_vec[:, Kp1] - u_vec[:, K])**2
@@ -120,6 +167,7 @@ def compute_beta(u_vec, p):
         return [beta0, beta1, beta2]
 
 def compute_weights(u_vec, p, eps, side):
+    """ Compute weights for the WENO scheme """
     dr = dr_coefficients(p)
     beta = compute_beta(u_vec, p)
     if p == 2:
@@ -141,47 +189,29 @@ def compute_weights(u_vec, p, eps, side):
             alfa_0 = dr[2] / (eps + beta[0])**2
             alfa_1 = dr[1] / (eps + beta[1])**2
             alfa_2 = dr[0] / (eps + beta[2])**2
+
         alfa_sum = alfa_0 + alfa_1 + alfa_2
         w = [alfa_0/alfa_sum, alfa_1/alfa_sum, alfa_2/alfa_sum]
+
         return w
 
 
-def left_BC():
-    rho = 1.
-    rho_u = 0.
-    E = 1./(gam - 1)
-    u_l = np.array([rho, rho_u, E]).reshape([-1, 1])
-    return u_l
-
-def right_BC():
-    rho = 0.125
-    rho_u = 0.
-    E = 0.1 /(gam - 1)
-    u_r = np.array([rho, rho_u, E]).reshape([-1, 1])
-    return u_r
-
-def add_BC_conditions_to_ghost_cells(u_vec, p):
-    """ Creates ghost cells on u vector depending on p degree of
-    interpolating polynomial """
-    if p == 2:
-        u_vec[:, 0] = left_BC()
-        u_vec[:, -1] = right_BC()
-    else:
-        u_vec[:, :3] = left_BC() * np.ones([3, 3])
-        u_vec[:, -3:] = right_BC() * np.ones([3, 3])
-
-    return u_vec
-
 def WENO5(u_vec, eps, p=3):
+    """
+    WENO scheme computes:
+        u_hat_minus
+        u_hat_plus
+        for a third order polynomial (p = 3)
+    """
     global K, Km2, Km1, Kp2, Kp1
 
-    # \hat_{u}_{k + 1/2}
+    # ------ \hat_{u}_{k + 1/2} ---------
     p0_r, p1_r, p2_r = compute_polynomials(u_vec, p, side='right')
     w0_r, w1_r, w2_r = compute_weights(u_vec, p, eps, side='right')
     u_hat_plus = w0_r * p0_r + w1_r * p1_r + w2_r * p2_r
 
-    # \hat_{u}_{k + 1 - 1/2}
-    u_vec_kp1 = u_vec[:, Kp1]
+    # ------ \hat_{u}_{k + 1 - 1/2} --------
+    u_vec_kp1 = u_vec[:, Kp1]  # Roll u_vec plus 1
     p0_l, p1_l, p2_l = compute_polynomials(u_vec_kp1, p, side='left')
     w0_l, w1_l, w2_l = compute_weights(u_vec_kp1, p, eps, side='left')
     u_hat_minus = w0_l * p0_l + w1_l * p1_l + w2_l * p2_l
@@ -193,6 +223,7 @@ def compute_LF_flux(u_minus, u_plus, alpha_LF):
     return flux
 
 def RK3_Integration(u_vec, ht, p, eps):
+    """ Three-stage Runge-Kutta integration of constructed u vectors """
     global Km1, Km2, K, Kp1, Kp2
 
     # --------- First stage -----------
@@ -250,6 +281,7 @@ if __name__ == '__main__':
     # --------------------------------------
     x, hx = np.linspace(0, 1, nx, endpoint=True, retstep=True)
     u_vec, P_vec = initial_fun(x)
+    u_vec_0, P_vec_0 = u_vec.copy(), P_vec.copy()
     l_v = compute_eigenvalues(u_vec)
     max_l_0 = compute_lambda_max(l_v)
     ht = CFL * hx / max_l_0
@@ -261,7 +293,9 @@ if __name__ == '__main__':
     Kp1 = np.roll(K, -1)    # 1, ..., nx
     Kp2 = np.roll(K, -2)    # 2, ..., nx + 1
 
-    # WENO simulation
+    # -------------------------------------
+    #               Computation
+    # --------------------------------------
     t_c = 0
     while t_c < T:
         # Tensor update
@@ -275,8 +309,29 @@ if __name__ == '__main__':
         max_l = compute_lambda_max(l_v)
         ht = CFL * hx / max_l
 
-    fig, ax = plt.subplots()
-    ax.plot(x, u_vec[0, :])
+    fig, ax = plt.subplots(2, 2)
+    plt.tight_layout()
+
+    ax[0, 0].plot(x, u_vec[0, :], c='b', label='T = {}'.format(str(T)))
+    ax[0, 0].plot(x, u_vec_0[0, :], c='g', label='T = {}'.format(0))
+    ax[0, 0].set_ylabel(r'$\rho$')
+
+    ax[0, 1].plot(x, u_vec[1, :] / u_vec[0, :], c='b', label='T = {}'.format(str(T)))
+    ax[0, 1].plot(x, u_vec_0[1, :] / u_vec_0[0, :], c='g', label='T = {}'.format(0))
+    ax[0, 1].set_ylabel('u')
+
+    ax[1, 0].plot(x, u_vec[2, :], c='b', label='T = {}'.format(str(T)))
+    ax[1, 0].plot(x, u_vec_0[2, :], c='g', label='T = {}'.format(0))
+    ax[1, 0].set_xlabel('x')
+    ax[1, 0].set_ylabel('E')
+
+    ax[1, 1].plot(x, P_vec, c='b', label='T = {}'.format(str(T)))
+    ax[1, 1].plot(x, P_vec_0, c='g', label='T = {}'.format(0))
+    ax[1, 1].set_xlabel('x')
+    ax[1, 1].set_ylabel('P')
+    ax[1, 1].legend(loc='best')
+
+
 
 
 
